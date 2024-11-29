de50ce4bb3a7326ffc94141dc9b7fd7495e039f5
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-29 16:11:40 +0100

    tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'light-purple':'#B37FE8',
+      'dark-purple':'#521690',
+      'black':'#555555',
+      'soft-black':'#333333',
+      'blue':'#47A1FF'
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
