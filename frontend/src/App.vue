166d55f4edd5ff3102761a73674496cecc4c9f20
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-12 11:02:49 +0100

    display only the rourteview

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <nav>
-      </nav>
-    </div>
-  </header>
-
+  
   <RouterView />
 </template>
 
