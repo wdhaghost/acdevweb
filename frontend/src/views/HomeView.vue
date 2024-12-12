6cfd72f8da147e3fa4af095b10288eba65a55c4c
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-12 11:05:19 +0100

    add: style to main section

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup>
+import Hero from '@/components/Hero.vue'
+import Header from  '@/components/Header.vue'
+import Services from  '@/components/Services.vue'
 </script>
 
 <template>
-  <main>
-
+  <main class="h-lvh">
+    <Header/>
+    <Hero/>
+    <Services/>
   </main>
 </template>
