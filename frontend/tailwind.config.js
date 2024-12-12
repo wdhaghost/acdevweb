336de8bbdf568d584f0d1b2ad2a5b59f2718c624
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-12 11:03:10 +0100

    tailwind config colors parameters

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,14 +5,18 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
+    
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // Définit Montserrat comme une classe personnalisée
+      },
+      colors:{
       'light-purple':'#B37FE8',
       'dark-purple':'#521690',
       'black':'#555555',
       'soft-black':'#333333',
       'blue':'#47A1FF'
     },
-    extend: {
     },
   },
   plugins: [],
