36298ea9bc18c28f03279495f08474066476f11a
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-12 11:52:48 +0100

    add: service card component

---
+++ frontend/src/components/service-card.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['title','text'])
+
+</script>
+<template>
+    <div class="w-full max-w-80 h-[300px] p-2.5 shadow-md">
+      <p class="font-montserrat font-semibold text-xl">
+        {{ props.title }}
+      </p>  
+      <p class="font-robotto text-lg">{{ props.text }}</p>
+    </div>
+</template>
