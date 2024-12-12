86d46fe39c23849f645d77aafff5c983018d2ae2
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-12 11:04:38 +0100

    add: hero section component

---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+<template>
+    <div class="flex w-full h-4/5 px-36 pt-10 pb-20">
+        <div class="flex flex-col gap-3 w-full h-full p-5">
+            <p class="text-6xl text-soft-black font-bold font-montserrat">Votre Projet web, clé en main!</p>
+            <p>Nous créons des sites vitrines et e-commerce adaptés à vos besoins, pour augmenter votre visibilité et vos ventes.</p>
+        </div>
+        <div class="w-full h-full bg-white opacity-30 shadow rounded"> 
+
+        </div>
+    </div>
+</template>
