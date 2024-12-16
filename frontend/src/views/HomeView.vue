b4f7c18b9ce093cef8d35af30459c3d0c88e4578
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-16 15:20:51 +0100

    add: contact Section to homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import Hero from '@/components/Hero.vue'
 import Header from  '@/components/Header.vue'
 import Services from  '@/components/Services.vue'
+import Contact from '@/components/Contact.vue';
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import Services from  '@/components/Services.vue'
     <Header/>
     <Hero/>
     <Services/>
+    <Contact/>
   </div>
 </template>
