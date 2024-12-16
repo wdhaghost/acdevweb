75a54f42f3825b52e81f619274f6f70962c10d33
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-16 16:08:05 +0100

    feat:  create input component

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['label','placeholder','type'])
+
+</script>
+<template>
+    <div class="grow">
+        <p class="font-montserrat font-bold text-foreground">{{ props.label }}</p>
+        <input class="w-full p-2.5 rounded-md " :type=props.type :placeholder=props.placeholder>
+    </div>
+</template>
