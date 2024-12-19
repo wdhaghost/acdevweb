02899677d1b4ad0dc7f4c0f97364d3fde4ffef76
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-19 16:49:51 +0100

    add: passing ref value as a props

--- frontend/src/components/TextArea.vue
+++ frontend/src/components/TextArea.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { defineProps } from 'vue';
-const props = defineProps(['label','placeholder','text'])
+const props = defineProps(['label','placeholder'])
+const value = defineModel('value')
 
 </script>
 <template>
-    <div class="grow">
+    <div class="w-full">
         <label class="font-montserrat font-bold text-foreground">{{ props.label }}</label>
-
-        <textarea class="w-full h- p-2.5 rounded-md text-black" :placeholder=props.placeholder ></textarea>
+        <textarea class="w-full min-h-[200px] h- p-2.5 rounded-md text-black" :placeholder=props.placeholder v-model="value" ></textarea>
     </div>
 </template>
