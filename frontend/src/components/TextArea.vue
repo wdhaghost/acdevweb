59fadc9c84ee524e57c072e0decc5c9868db1d4f
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-19 15:06:20 +0100

    feat: textarea component created

---
+++ frontend/src/components/TextArea.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['label','placeholder','text'])
+
+</script>
+<template>
+    <div class="grow">
+        <label class="font-montserrat font-bold text-foreground">{{ props.label }}</label>
+
+        <textarea class="w-full h- p-2.5 rounded-md text-black" :placeholder=props.placeholder ></textarea>
+    </div>
+</template>
