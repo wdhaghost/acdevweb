1c3f5636054d6e996f3cc26a826269fad1335d02
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-19 15:57:54 +0100

    fix: change style of background for bright and dark mode

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -3,10 +3,10 @@
 <template>
     <div class="flex w-full h-4/5 px-36 pt-10 pb-20">
         <div class="flex flex-col gap-3 w-full h-full p-5">
-            <p class="text-6xl text-soft-black font-bold font-montserrat">Votre Projet web, clé en main!</p>
+            <p class="text-6xl text-foreground font-bold font-montserrat">Votre Projet web, clé en main!</p>
             <p class="text-2xl">Nous créons des sites vitrines et e-commerce adaptés à vos besoins, pour augmenter votre visibilité et vos ventes.</p>
         </div>
-        <div class="w-full h-full bg-white opacity-30 shadow rounded"> 
+        <div class="w-full h-full bg-background backdrop-opacity-30 shadow rounded"> 
 
         </div>
     </div>
