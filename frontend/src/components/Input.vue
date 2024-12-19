720eb4273bba67371c010f4313d1035fda9571b6
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-19 14:40:17 +0100

    style: text of input in black

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -6,6 +6,6 @@ const props = defineProps(['label','placeholder','type'])
 <template>
     <div class="grow">
         <p class="font-montserrat font-bold text-foreground">{{ props.label }}</p>
-        <input class="w-full p-2.5 rounded-md " :type=props.type :placeholder=props.placeholder>
+        <input class="w-full p-2.5 rounded-md text-black" :type=props.type :placeholder=props.placeholder>
     </div>
 </template>
